810302a7969b2506ee92968cc8f9aea72f1a9851
Jason <murangira99@gmail.com>
2024-04-29 10:24:19 +0200

    added importf

--- client/src/App.js
+++ client/src/App.js
@@ -1,2 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import PostList from './PostList'; // Import PostList component
+import CreatePost from './CreatePost'; // Import CreatePost component
+import UpdatePost from './UpdatePost'; // Import UpdatePost component
+
