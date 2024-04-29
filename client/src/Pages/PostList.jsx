88431f89959c001fec6a2604da81fc220305af63
Jason <murangira99@gmail.com>
2024-04-29 11:14:10 +0200

    added constlist

---
+++ client/src/Pages/PostList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './Post';
+
+const PostList = ({ posts, onUpdate, onDelete }) => {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <Post key={post.id} post={post} onUpdate={onUpdate} onDelete={onDelete} />
+      ))}
+    </ul>
+  );
+};
+
+export default PostList;
