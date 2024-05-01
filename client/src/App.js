4690581cc0a2401710470df8244adcdadf1397b3
Jason <murangira99@gmail.com>
2024-05-01 19:42:39 +0200

    added axios pn home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";  
-import Add from "./pages/Add";
-import Books from "./pages/Books";
-import Update from "./pages/Update";
-
+import CreatePost from "./pages/CreatePost";
+import Home from "./pages/Home";
+import UpdatePost from "./pages/UpdatePost";
+import PostList from "./pages/PostList";
 
 
 function App() {
-
-return (
-
-  <div className="App">
-hellow world
-  </div>
-);
-
-
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/CreatePost" element={<CreatePost />} />
+          <Route path="/UpdatePost" element={<UpdatePost />} />
+          <Route path="/PostList" element={<PostList />} />
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
