7883015af6bc8b5963ee629589c44ea182765352
Jason <murangira99@gmail.com>
2024-05-08 21:11:45 +0200

    added ;

--- client/src/Pages/CreatePost.jsx
+++ client/src/Pages/CreatePost.jsx
@@ -1,7 +1,7 @@
 // Import necessary modules
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import './CreatePost.css';
 
 const CreatePost = () => {
